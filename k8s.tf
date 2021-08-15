42efe7eca9cd510d152c1dac553d2283f271c1de
Abhilash KS <40015815+ababhilash@users.noreply.github.com>
2021-08-15 18:01:25 +0530

    Create k8s.tf

---
+++ k8s.tf
@@ -0,0 +1,21 @@
+resource "google_container_cluster" "primary" {
+  name               = "myk8scluster"
+  location           = var.gcp_region
+  initial_node_count = 1
+
+  network    = google_compute_network.myvpc.name
+  subnetwork = google_compute_subnetwork.network.name
+
+  node_config {
+    oauth_scopes = [
+      "https://www.googleapis.com/auth/logging.write",
+      "https://www.googleapis.com/auth/monitoring",
+    ]
+
+    metadata = {
+      disable-legacy-endpoints = "true"
+    }
+    
+  }
+ 
+}
