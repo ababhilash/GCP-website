0c70e92e8eb507ccba4ad06970ea46eb5de5ddda
Abhilash KS <40015815+ababhilash@users.noreply.github.com>
2021-08-15 18:00:40 +0530

    Create firewall.tf

---
+++ firewall.tf
@@ -0,0 +1,14 @@
+resource "google_compute_firewall" "rule" {
+  name    = "myfirewall"
+  network = google_compute_network.myvpc.name
+
+  allow {
+    protocol = "icmp"
+  }
+
+  allow {
+    protocol = "tcp"
+    ports    = ["80"]
+  }
+
+}
